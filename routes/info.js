bb081156f68081ad95fd711b168ca41cf3340209
Sangeun Lee <dltahazh@gmail.com>
2019-04-18 20:30:01 +0900

    구 정보 수정

--- routes/info.js
+++ routes/info.js
@@ -14,7 +14,7 @@ router.get('/:lat/:long', async(req, res) => {
   let arr = ['강남구','강동구','강북구','강서구','관악구','광진구','구로구','금천구','노원구','도봉구','동대문구','동작구','마포구','서대문구','서초구','성동구','성북구','송파구','양천구','영등포구','용산구','은평구','종로구','중구','중랑구'];
   let weatherArr = [[61, 126],[62, 126],[61, 128],[58, 126],[59, 125],[62, 126],[58, 125],[59, 124],[61, 129],[61, 129],[61, 127],[59, 125],[59, 127],[59, 127],[61, 125],[61, 127],[61, 127],[62, 126],[58, 126],[58, 126],[60, 126],[59, 127],[60, 127],[60, 127],[62, 128]];
 
-
+  let TEMP = "서울특별시 강남구 테헤란로 107"
   let result = TEMP.split(' ');
   let code = -1;
 
@@ -66,7 +66,7 @@ router.get('/:lat/:long', async(req, res) => {
   	let selectdust = await dust.find({"MSRSTE_NM" : arr[code], "MSRDT" : date});
     let selectweather = await weather.find({"MSRSTE_NM" : arr[code], "MSRDT" : base_date + base_time});
   	
-  	if (!selectdust && !selectweather) {
+  	if (selectdust.length === 0 && selectweather.length === 0) {
       res.status(500).send({
         message : "Internal Server Error"
       });
