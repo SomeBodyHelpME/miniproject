3e6b273d6e0ebe8d070708fe2253fcdf32d246f8
윤찬명 <chan@woowahan.com>
2019-04-26 20:40:53 +0900

    Add region's info function

--- models/dust.js
+++ models/dust.js
@@ -2,16 +2,22 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const dustSchema = new Schema({
-	MSRDT : String,
-	MSRSTE_NM : String,
-	NO2 : Number,
-	O3 : Number,
-	CO : Number,
-	SO2 : Number,
-	PM10 : Number,
-	PM25 : Number
+	MSRDT: String,
+	MSRSTE_NM: String,
+	NO2: Number,
+	O3: Number,
+	CO: Number,
+	SO2: Number,
+	PM10: Number,
+	PM25: Number
 });
 
-dustSchema.index({"MSRSTE_NM": "text"})
+dustSchema.statics.search = function (regionName) {
+	return this.findOne({ $text: { $search: regionName } }, null, {
+		sort: { MSRDT: -1 }
+	});
+}
+
+dustSchema.index({ "MSRSTE_NM": "text" })
 
 module.exports = mongoose.model('dusts', dustSchema);
--- routes/region.js
+++ routes/region.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Region = require("../models/region");
+const Dust = require("../models/dust");
+const Weather = require("../models/weather");
 
 router.get("/", async (req, res) => {
   const { keyword } = req.query;
@@ -25,11 +27,24 @@ router.get("/", async (req, res) => {
     });
 });
 
-// router.get('/:regionId/info', async (req, res) => {
-//     const regionId = req.param.regionId
+router.get("/:regionId/info", async (req, res) => {
+  const regionId = req.params.regionId;
 
-//     const region = await Region.findById(regionId)
-// })
+  if (regionId.length !== 24) {
+    res.status(400).json({ msg: "ObjectId가 아니네." });
+    return;
+  }
+  const region = await Region.findById(regionId);
 
+  if (region === null) {
+    res.status(404).json({ msg: "그런 지역 없다.." });
+    return;
+  }
+
+  const dust = await Dust.search(region.properties.adm_nm);
+  const weather = await Weather.search(region.properties.adm_nm);
+
+  res.status(200).json({ dust, weather });
+});
 
 module.exports = router;
